6d32050aa925c82d7e5c8795a39c6413d89c16c2
Marc Roettig <marc.roettig@googlemail.com>
2011-10-13 13:31:26 +0200

    finalized ExternalToolsDBPage.

--- src/org/ballproject/knime/base/preferences/GKNExternalToolsPage.java
+++ src/org/ballproject/knime/base/preferences/GKNExternalToolsPage.java
@@ -8,21 +8,18 @@ import java.util.Map;
 import org.ballproject.knime.GenericNodesPlugin;
 import org.ballproject.knime.base.external.ExtToolDB;
 import org.ballproject.knime.base.external.ExtToolDB.ExternalTool;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
-import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
+
 
 public class GKNExternalToolsPage extends PreferencePage implements IWorkbenchPreferencePage 
 {
@@ -39,38 +36,6 @@ public class GKNExternalToolsPage extends PreferencePage implements IWorkbenchPr
 	public void init(IWorkbench wb)
 	{
 	}
-
-	/*
-	@Override
-	protected void createFieldEditors()
-	{
-		Composite parent = getFieldEditorParent();
-		
-		final ScrolledComposite sc = new ScrolledComposite(parent, SWT.V_SCROLL);
-		sc.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		
-		Composite c = new Composite(sc, SWT.NONE);
-		FillLayout fillLayout = new FillLayout();
-		fillLayout.type = SWT.VERTICAL;
-		c.setLayout(fillLayout);
-
-		sc.setContent(c);
-	    sc.setExpandHorizontal(true);
-	    sc.setExpandVertical(true);
-	    
-	    Map<String,List<ExternalTool>> plugin2tools = ExtToolDB.getInstance().getToolsByPlugin();
-	    
-	    for(String pluginname: plugin2tools.keySet())
-	    {
-	    	Label label = new Label (c, SWT.NONE);
-			label.setText (pluginname);
-	    	for(ExternalTool tool: plugin2tools.get(pluginname))
-			{
-	    		addField(new FileFieldEditor(tool.getKey(), tool.getToolname(), c));	
-			}	
-	    }
-	}
-	*/
 
 	@Override
 	protected Control createContents(Composite parent)
@@ -96,13 +61,19 @@ public class GKNExternalToolsPage extends PreferencePage implements IWorkbenchPr
 	    	for(ExternalTool tool: plugin2tools.get(pluginname))
 			{
 	    		String[] toks = pluginname.split("\\.");
-	    		String name = toks[toks.length-1]+" - "+tool.getToolname(); 
-	    		FileFieldEditor toolpath = new FileFieldEditor(tool.getKey(), name, c);	
+	    		
+	    		String name = tool.getToolname();
+	    		if(toks!=null)
+	    			name = toks[toks.length-1]+" - "+tool.getToolname();
+	    		
+	    		FileFieldEditor toolpath = new FileFieldEditor(tool.getKey(), name, c);
+	    		
 	    		toolpath.setPreferenceStore(getPreferenceStore());
 	    		toolpath.load();
 	    		String val = preferenceStore.getString(toolpath.getPreferenceName());
 	    		toolpath.setStringValue((val==null?"":val));
 	    		toolpathes.add(toolpath);
+	    		extTools.add(tool);
 			}	
 	    }
 	    
@@ -110,20 +81,39 @@ public class GKNExternalToolsPage extends PreferencePage implements IWorkbenchPr
 	}
 	
 	private List<FileFieldEditor> toolpathes = new ArrayList<FileFieldEditor>();
+	private List<ExternalTool>    extTools   = new ArrayList<ExternalTool>();
 	
 	@Override
 	public boolean performOk() 
 	{
 	    // Get the preference store
 	    IPreferenceStore preferenceStore = getPreferenceStore();
-
+	    
+	    int idx = 0;
 	    for(FileFieldEditor fe : toolpathes)
 	    {
+	    	ExtToolDB.getInstance().setToolPath(extTools.get(idx), fe.getStringValue());
 	    	preferenceStore.setValue(fe.getPreferenceName(),fe.getStringValue());
 	    }
 
 	    // Return true to allow dialog to close
 	    return true;
-	  }
+	}
+
+	@Override
+	protected void performApply()
+	{
+	    // Get the preference store
+	    IPreferenceStore preferenceStore = getPreferenceStore();
+	    
+	    int idx = 0;
+	    for(FileFieldEditor fe : toolpathes)
+	    {
+	    	ExtToolDB.getInstance().setToolPath(extTools.get(idx), fe.getStringValue());
+	    	preferenceStore.setValue(fe.getPreferenceName(),fe.getStringValue());
+	    }
+	}
+	
+	
 
 }
