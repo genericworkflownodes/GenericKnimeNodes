5edf312cce625c3bf6472e9005b600ff2f115a01
Marc Roettig <marc.roettig@googlemail.com>
2011-10-13 18:33:35 +0200

    adjusted Demangler interface.

--- src/org/ballproject/knime/base/mime/demangler/Demangler.java
+++ src/org/ballproject/knime/base/mime/demangler/Demangler.java
@@ -1,16 +1,18 @@
 package org.ballproject.knime.base.mime.demangler;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.Iterator;
 
 import org.ballproject.knime.base.mime.MIMEFileCell;
 import org.knime.core.data.DataCell;
 import org.knime.core.data.DataType;
+import org.knime.core.data.url.MIMEType;
 
 public interface Demangler extends Serializable
 {
-	DataType getSourceType();
+	MIMEType getMIMEType();
 	DataType getTargetType();
-	Iterator<DataCell> demangle(MIMEFileCell cell);
+	Iterator<DataCell> demangle(URI file);
 	MIMEFileCell mangle(Iterator<DataCell> iter);
 }
