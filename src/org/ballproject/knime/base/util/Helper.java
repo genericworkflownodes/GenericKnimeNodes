4df913cfdcb88f16cb0640f69f900b7fd755f685
Björn Kahlert <mail@bkahlert.com>
2012-03-12 03:27:09 +0100

    - An optional directory can now be passed to the node generator which is then used as the directory in where to look for plugin.properties. Otherwise the current working directory is used. - The payload directory must now reside in the plugin.properties' directory. - The optional descriptors directory must now reside in the plugin.properties' directory. - The optional executables directory must now reside in functionality removed. - Added tons of typing. - Reduced NodeGenerator so ~250LOC.
    Much of testing needed though.

--- src/org/ballproject/knime/base/util/Helper.java
+++ src/org/ballproject/knime/base/util/Helper.java
@@ -32,207 +32,184 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Helper
-{
-	public static class OS
-	{
-		public static int WIN  = 0;
-		public static int MAC  = 1;
+public class Helper {
+	public static class OS {
+		public static int WIN = 0;
+		public static int MAC = 1;
 		public static int UNIX = 2;
 	}
-	
-	public static int getOS()
-	{
-		String os   = System.getProperty("os.name");
-		
-		// might be to lax
-		
-		if(os.toLowerCase().contains("nux")||os.toLowerCase().contains("nix"))
-		{
+
+	public static int getOS() {
+		String os = System.getProperty("os.name");
+
+		// TODO might be to lax
+		if (os.toLowerCase().contains("nux")
+				|| os.toLowerCase().contains("nix")) {
 			return OS.UNIX;
 		}
-		if(os.toLowerCase().contains("mac"))
-		{
+		if (os.toLowerCase().contains("mac")) {
 			return OS.MAC;
 		}
-		
+
 		return OS.WIN;
 	}
 
-	public static List<String> readStringsFromStream(InputStream in) throws IOException
-	{
+	public static List<String> readStringsFromStream(InputStream in)
+			throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		
+
 		List<String> ret = new ArrayList<String>();
 		String line = "";
-		 
-		while( (line=reader.readLine())!=null)
-		{
+
+		while ((line = reader.readLine()) != null) {
 			ret.add(line.trim());
 		}
 		reader.close();
-		
+
 		return ret;
 	}
-	
-	public static String getExecutableName(String nodename, String path)
-	{
-		String test = path+File.separator+nodename; 
-		if(new File(test).exists())
+
+	public static String getExecutableName(String nodename, String path) {
+		String test = path + File.separator + nodename;
+		if (new File(test).exists())
 			return test;
-		
-		test = path+File.separator+nodename+".bin";
-		if(new File(test).exists())
+
+		test = path + File.separator + nodename + ".bin";
+		if (new File(test).exists())
 			return test;
-		
-		test = path+File.separator+nodename+".exe";
-		if(new File(test).exists())
+
+		test = path + File.separator + nodename + ".exe";
+		if (new File(test).exists())
 			return test;
-		
+
 		return null;
 	}
 
-	static public boolean deleteDirectory(File path)
-	{
-		if (path.exists())
-		{
+	static public boolean deleteDirectory(File path) {
+		if (path.exists()) {
 			File[] files = path.listFiles();
-			for (int i = 0; i < files.length; i++)
-			{
-				if (files[i].isDirectory())
-				{
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isDirectory()) {
 					deleteDirectory(files[i]);
-				}
-				else
-				{
+				} else {
 					files[i].delete();
 				}
 			}
 		}
 		return (path.delete());
 	}
-	
-	public static void copyStream(InputStream in, File dest) throws IOException
-	{
-		FileOutputStream    out = new FileOutputStream(dest);
+
+	public static void copyStream(InputStream in, File dest) throws IOException {
+		FileOutputStream out = new FileOutputStream(dest);
 		BufferedInputStream bin = new BufferedInputStream(in);
 		byte[] buffer = new byte[2048];
 		int len;
- 		while((len=bin.read(buffer, 0, 2048))!=-1)
- 		{
- 			out.write(buffer,0,len);
- 		}
- 		out.close();
- 		bin.close();
+		while ((len = bin.read(buffer, 0, 2048)) != -1) {
+			out.write(buffer, 0, len);
+		}
+		out.close();
+		bin.close();
 	}
-	
-	public static void copyFile(File in, File out) throws IOException
-	{
+
+	public static void copyFile(File in, File out) throws IOException {
 		FileChannel inChannel = new FileInputStream(in).getChannel();
 		FileChannel outChannel = new FileOutputStream(out).getChannel();
-		try
-		{
+		try {
 			inChannel.transferTo(0, inChannel.size(), outChannel);
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			throw e;
-		}
-		finally
-		{
+		} finally {
 			if (inChannel != null)
 				inChannel.close();
 			if (outChannel != null)
 				outChannel.close();
 		}
 	}
-	
+
 	static private Random rng = new Random();
-	
-	static synchronized public String getTemporaryFilename(String directory, String suffix, boolean autodelete) throws IOException
-	{
-		
+
+	static synchronized public String getTemporaryFilename(String directory,
+			String suffix, boolean autodelete) throws IOException {
+
 		int num = Math.abs(rng.nextInt());
-		File f = new File(directory+File.separator+String.format("%06d.%s",num,suffix));
-		while(f.exists())
-		{
+		File f = new File(directory + File.separator
+				+ String.format("%06d.%s", num, suffix));
+		while (f.exists()) {
 			num = Math.abs(rng.nextInt());
-			f = new File(directory+File.separator+String.format("%06d.%s",num,suffix));
+			f = new File(directory + File.separator
+					+ String.format("%06d.%s", num, suffix));
 		}
 		f.createNewFile();
-		
-		if(autodelete)
+
+		if (autodelete)
 			f.deleteOnExit();
-		
+
 		return f.getAbsolutePath();
 	}
-	
-	static synchronized public String getRelativeTemporaryFilename(String directory, String suffix, boolean autodelete) throws IOException
-	{
-		
+
+	static synchronized public String getRelativeTemporaryFilename(
+			String directory, String suffix, boolean autodelete)
+			throws IOException {
+
 		int num = Math.abs(rng.nextInt());
-		File f = new File(directory+File.separator+String.format("%06d.%s",num,suffix));
-		while(f.exists())
-		{
+		File f = new File(directory + File.separator
+				+ String.format("%06d.%s", num, suffix));
+		while (f.exists()) {
 			num = Math.abs(rng.nextInt());
-			f = new File(directory+File.separator+String.format("%06d.%s",num,suffix));
+			f = new File(directory + File.separator
+					+ String.format("%06d.%s", num, suffix));
 		}
 		f.createNewFile();
-		
-		if(autodelete)
+
+		if (autodelete)
 			f.deleteOnExit();
-		
+
 		return f.getName();
 	}
-	
-	static synchronized public String getTemporaryFilename(String suffix, boolean autodelete) throws IOException
-	{
-		return getTemporaryFilename(System.getProperty("java.io.tmpdir"),suffix,autodelete);
+
+	static synchronized public String getTemporaryFilename(String suffix,
+			boolean autodelete) throws IOException {
+		return getTemporaryFilename(System.getProperty("java.io.tmpdir"),
+				suffix, autodelete);
 	}
-	
-	static synchronized public String getTemporaryDirectory(String directory, String prefix, boolean autodelete) throws IOException
-	{
-		
+
+	static synchronized public String getTemporaryDirectory(String directory,
+			String prefix, boolean autodelete) throws IOException {
+
 		int num = Math.abs(rng.nextInt());
-		File f = new File(directory+File.separator+String.format("%s%06d",prefix,num));
-		while(f.exists())
-		{
+		File f = new File(directory + File.separator
+				+ String.format("%s%06d", prefix, num));
+		while (f.exists()) {
 			num = Math.abs(rng.nextInt());
-			f = new File(directory+File.separator+String.format("%s%06d",prefix,num));
+			f = new File(directory + File.separator
+					+ String.format("%s%06d", prefix, num));
 		}
 		f.mkdirs();
-		
-		if(autodelete)
+
+		if (autodelete)
 			f.deleteOnExit();
-		
+
 		return f.getAbsolutePath();
 	}
-	
-	static synchronized public String getTemporaryDirectory(String prefix, boolean autodelete) throws IOException
-	{
-		return getTemporaryDirectory(System.getProperty("java.io.tmpdir"),prefix,autodelete);
+
+	static synchronized public String getTemporaryDirectory(String prefix,
+			boolean autodelete) throws IOException {
+		return getTemporaryDirectory(System.getProperty("java.io.tmpdir"),
+				prefix, autodelete);
 	}
-	
-	public static Object createObject(String className) 
-	{
-	      Object object = null;
-	      try 
-	      {
-	          Class classDefinition = Class.forName(className);
-	          object = classDefinition.newInstance();
-	      } 
-	      catch (InstantiationException e) 
-	      {
-	          e.printStackTrace();
-	      } 
-	      catch (IllegalAccessException e) 
-	      {
-	    	  e.printStackTrace();
-	      } 
-	      catch (ClassNotFoundException e) 
-	      {
-	    	  e.printStackTrace();
-	      }
-	      return object;
+
+	public static Object createObject(String className) {
+		Object object = null;
+		try {
+			Class<?> classDefinition = Class.forName(className);
+			object = classDefinition.newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return object;
 	}
 }
