1dd2e8e868f98bebf0e938c176dcf576f8a71f6d
Björn Kahlert <mail@bkahlert.com>
2012-03-26 19:59:08 +0200

    second approach * finished project split  * restructured build.xml to reflect project split  * included .classpath, .project into assets, to construct fully functional knime node extension projects using node_generator

--- com.genericworkflownodes.knime.base_plugin/src/org/ballproject/knime/nodegeneration/model/files/INodeConfigurationFile.java
+++
@@ -1,7 +0,0 @@
-package org.ballproject.knime.nodegeneration.model.files;
-
-import org.ballproject.knime.base.config.INodeConfiguration;
-
-public interface INodeConfigurationFile {
-	public INodeConfiguration getNodeConfiguration();
-}
