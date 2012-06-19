65b335e059aa633a5e20415640436f186032a431
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-06-19 14:30:32 +0200

    FileMergerNode to its own package

--- com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/nodes/merger/FileMergerNodeView.java
+++ com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/nodes/merger/FileMergerNodeView.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.genericworkflownodes.knime.nodes;
+package com.genericworkflownodes.knime.nodes.merger;
 
 import org.knime.core.node.NodeView;
 
