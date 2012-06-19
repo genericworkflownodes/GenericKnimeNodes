8275d8d43b7f423ac379c03661e74091c3053483
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-06-19 14:23:25 +0200

    1st version of working merge node

---
+++ com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/nodes/FileMergerNodeView.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2012, Stephan Aiche.
+ *
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.genericworkflownodes.knime.nodes;
+
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeView</code> for the "FileMerger" Node. This nodes takes two files
+ * (file lists) as input and outputs a merged list of both inputs.
+ * 
+ * @author aiche
+ */
+public class FileMergerNodeView extends NodeView<FileMergerNodeModel> {
+
+	/**
+	 * Creates a new view.
+	 * 
+	 * @param nodeModel
+	 *            The model (class: {@link FileMergerNodeModel})
+	 */
+	protected FileMergerNodeView(final FileMergerNodeModel nodeModel) {
+		super(nodeModel);
+
+		// TODO instantiate the components of the view here.
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void modelChanged() {
+
+		// TODO retrieve the new model from your nodemodel and
+		// update the view.
+		FileMergerNodeModel nodeModel = (FileMergerNodeModel) getNodeModel();
+		assert nodeModel != null;
+
+		// be aware of a possibly not executed nodeModel! The data you retrieve
+		// from your nodemodel could be null, emtpy, or invalid in any kind.
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onClose() {
+
+		// TODO things to do when closing the view
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onOpen() {
+
+		// TODO things to do when opening the view
+	}
+
+}
