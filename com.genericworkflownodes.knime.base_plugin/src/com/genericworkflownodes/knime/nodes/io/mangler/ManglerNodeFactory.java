a8323975d2e706b0907ff8f8cfed8ee169bc4a9a
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-07-31 16:34:58 +0200

    * moved io node code to new package  * implemented list view for imported files

---
+++ com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/nodes/io/mangler/ManglerNodeFactory.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2011, Marc Röttig.
+ *
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.genericworkflownodes.knime.nodes.io.mangler;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "Mangler" Node.
+ * 
+ * 
+ * @author roettig
+ */
+public class ManglerNodeFactory extends NodeFactory<ManglerNodeModel> {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ManglerNodeModel createNodeModel() {
+		return new ManglerNodeModel();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getNrNodeViews() {
+		return 0;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public NodeView<ManglerNodeModel> createNodeView(final int viewIndex,
+			final ManglerNodeModel nodeModel) {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean hasDialog() {
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public NodeDialogPane createNodeDialogPane() {
+		return new ManglerNodeDialog(new Object());
+	}
+
+}
