a0821a0a197eed8beb49dfafeea2fc478214a314
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-09-05 13:45:13 +0200

    Removed remaining references to ref_name
    fixes tests and samples

--- com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/cliwrapper/CLIMapping.java
+++ com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/cliwrapper/CLIMapping.java
@@ -37,7 +37,7 @@ package com.genericworkflownodes.knime.cliwrapper;
  * &lt;complexType name="mappingType">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;attribute name="ref_name" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="referenceName" type="{http://www.w3.org/2001/XMLSchema}string" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
