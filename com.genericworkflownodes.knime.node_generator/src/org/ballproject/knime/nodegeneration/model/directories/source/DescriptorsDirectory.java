da8207c11b8957b18a2800c7b7c2fdc6a7c0ac4e
Stephan Aiche <stephan.aiche@fu-berlin.de>
2012-11-08 14:27:37 +0100

    Addendum to merge.

--- com.genericworkflownodes.knime.node_generator/src/org/ballproject/knime/nodegeneration/model/directories/source/DescriptorsDirectory.java
+++ com.genericworkflownodes.knime.node_generator/src/org/ballproject/knime/nodegeneration/model/directories/source/DescriptorsDirectory.java
@@ -93,7 +93,6 @@ public class DescriptorsDirectory extends Directory {
 							e.getMessage()));
 					// throw new IOException("Error reading " + file.getPath(),
 					// e);
-					throw new IOException("Error reading " + file.getPath(), e);
 				} catch (Exception e) {
 					throw new IOException("Error reading " + file.getPath(), e);
 				}
