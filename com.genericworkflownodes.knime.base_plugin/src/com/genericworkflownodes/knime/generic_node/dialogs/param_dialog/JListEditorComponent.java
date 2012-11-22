genericworkflownodes/GenericKnimeNodes

commit 80c548442ef975586de9b2c28140f0de012b34fb

fixed problem of unsaved values when closing ListEditor

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/generic_node/dialogs/param_dialog/list_editor/ListEditorDialog.java
@@ -136,6 +136,7 @@ public class ListEditorDialog extends JDialog {
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				table.getCellEditor().stopCellEditing();
 				model.transferToParameter();
 				ListEditorDialog.this.dispose();
 			}

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/generic_node/dialogs/param_dialog/list_editor/ListCellEditor.java
@@ -117,6 +117,7 @@ public class ListCellEditor extends AbstractCellEditor implements
 					&& slp.getRestrictions().length > 0) {
 				choiceComboBox = new JComboBox(slp.getRestrictions());
 				choiceComboBox.setSelectedItem(value);
+
 				return choiceComboBox;
 			} else {
 				field = new JTextField(value.toString());

commit b571ddab790789e1b7778e8f90ba523b903833a8

improved dialog title and fixed small bug introduced in 80c548442ef975586de9b2c28140f0de012b34fb

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/generic_node/dialogs/param_dialog/JListEditorComponent.java
@@ -49,6 +49,7 @@ public class JListEditorComponent extends JLabel {
 		 */
 		ListEditorDialog led = new ListEditorDialog(p);
 		led.setVisible(true);
+
 		UIHelper.simulateEnterKeyPressed(this, 50);
 	}
 

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/generic_node/dialogs/param_dialog/list_editor/ListEditorDialog.java
@@ -37,6 +37,7 @@ import javax.swing.event.ListSelectionListener;
 
 import com.genericworkflownodes.knime.generic_node.dialogs.UIHelper;
 import com.genericworkflownodes.knime.parameter.ListParameter;
+import com.genericworkflownodes.knime.parameter.Parameter;
 
 /**
  * ListEditor dialog for StringList and NumericalList parameters.
@@ -52,7 +53,7 @@ public class ListEditorDialog extends JDialog {
 	 */
 	private static final long serialVersionUID = 9130341015527518732L;
 	private ListParameter parameter;
-	private final static String TITLE = "List Editor";
+	private final static String TITLE = "List Editor <%s> (%s)";
 	private ListEditorDialogModel model;
 	private JTable table;
 	private ListCellEditor cellEditor;
@@ -62,7 +63,8 @@ public class ListEditorDialog extends JDialog {
 	 */
 	public ListEditorDialog(ListParameter p) {
 		super();
-		setTitle(TITLE);
+		setTitle(String.format(TITLE, ((Parameter<?>) p).getKey(),
+				((Parameter<?>) p).getMnemonic()));
 		init(p);
 	}
 
@@ -136,7 +138,8 @@ public class ListEditorDialog extends JDialog {
 		okButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				table.getCellEditor().stopCellEditing();
+				if (table.isEditing())
+					table.getCellEditor().stopCellEditing();
 				model.transferToParameter();
 				ListEditorDialog.this.dispose();
 			}