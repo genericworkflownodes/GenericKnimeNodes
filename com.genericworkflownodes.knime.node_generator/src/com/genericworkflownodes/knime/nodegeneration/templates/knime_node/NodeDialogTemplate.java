genericworkflownodes/GenericKnimeNodes

commit b85123d1520d6297e86095227614e3d876fb5cf5

integrated startup extension into the node generated and made some updates and reorganizations in order to improve the node generator and the generated code

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeDialogTemplate.java
@@ -1,15 +1,17 @@
-package com.genericworkflownodes.knime.nodegeneration.templates;
+package com.genericworkflownodes.knime.nodegeneration.templates.knime_node;
 
 import java.io.IOException;
 
 import com.genericworkflownodes.knime.nodegeneration.NodeGenerator;
+import com.genericworkflownodes.knime.nodegeneration.templates.Template;
 
 public class NodeDialogTemplate extends Template {
 
 	public NodeDialogTemplate(String packageName, String nodeName)
 			throws IOException {
-		super(NodeGenerator.class
-				.getResourceAsStream("templates/NodeDialog.template"));
+		super(
+				NodeGenerator.class
+						.getResourceAsStream("templates/knime_nodes/NodeDialog.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeFactoryTemplate.java
@@ -1,15 +1,17 @@
-package com.genericworkflownodes.knime.nodegeneration.templates;
+package com.genericworkflownodes.knime.nodegeneration.templates.knime_node;
 
 import java.io.IOException;
 
 import com.genericworkflownodes.knime.nodegeneration.NodeGenerator;
+import com.genericworkflownodes.knime.nodegeneration.templates.Template;
 
 public class NodeFactoryTemplate extends Template {
 
 	public NodeFactoryTemplate(String packageName, String nodeName)
 			throws IOException {
-		super(NodeGenerator.class
-				.getResourceAsStream("templates/NodeFactory.template"));
+		super(
+				NodeGenerator.class
+						.getResourceAsStream("templates/knime_nodes/NodeFactory.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeFactoryXMLTemplate.java
@@ -1,13 +1,13 @@
-package com.genericworkflownodes.knime.nodegeneration.templates;
+package com.genericworkflownodes.knime.nodegeneration.templates.knime_node;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
 import com.genericworkflownodes.knime.config.INodeConfiguration;
 import com.genericworkflownodes.knime.nodegeneration.NodeGenerator;
+import com.genericworkflownodes.knime.nodegeneration.templates.Template;
 import com.genericworkflownodes.knime.parameter.Parameter;
 import com.genericworkflownodes.knime.port.Port;
 
@@ -158,8 +158,9 @@ public class NodeFactoryXMLTemplate extends Template {
 	public NodeFactoryXMLTemplate(final String nodeName,
 			final INodeConfiguration nodeConfiguration, final String iconPath)
 			throws IOException {
-		super(NodeGenerator.class
-				.getResourceAsStream("templates/NodeXMLDescriptor.template"));
+		super(
+				NodeGenerator.class
+						.getResourceAsStream("templates/knime_nodes/NodeXMLDescriptor.template"));
 
 		replace("__ICON__", iconPath);
 		replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeModelTemplate.java
@@ -1,11 +1,11 @@
-package com.genericworkflownodes.knime.nodegeneration.templates;
+package com.genericworkflownodes.knime.nodegeneration.templates.knime_node;
 
 import java.io.IOException;
-
 
 import com.genericworkflownodes.knime.config.INodeConfiguration;
 import com.genericworkflownodes.knime.nodegeneration.NodeGenerator;
 import com.genericworkflownodes.knime.nodegeneration.exceptions.UnknownMimeTypeException;
+import com.genericworkflownodes.knime.nodegeneration.templates.Template;
 import com.genericworkflownodes.knime.port.Port;
 
 public class NodeModelTemplate extends Template {
@@ -13,8 +13,9 @@ public class NodeModelTemplate extends Template {
 	public NodeModelTemplate(String packageName, String nodeName,
 			INodeConfiguration nodeConfiguration) throws IOException,
 			UnknownMimeTypeException {
-		super(NodeGenerator.class
-				.getResourceAsStream("templates/NodeModel.template"));
+		super(
+				NodeGenerator.class
+						.getResourceAsStream("templates/knime_nodes/NodeModel.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeViewTemplate.java
@@ -1,15 +1,16 @@
-package com.genericworkflownodes.knime.nodegeneration.templates;
+package com.genericworkflownodes.knime.nodegeneration.templates.knime_node;
 
 import java.io.IOException;
 
 import com.genericworkflownodes.knime.nodegeneration.NodeGenerator;
+import com.genericworkflownodes.knime.nodegeneration.templates.Template;
 
 public class NodeViewTemplate extends Template {
 
 	public NodeViewTemplate(String packageName, String nodeName)
 			throws IOException {
 		super(NodeGenerator.class
-				.getResourceAsStream("templates/NodeView.template"));
+				.getResourceAsStream("templates/knime_nodes/NodeView.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/custom/GenericActivator.java
@@ -33,6 +33,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 import com.genericworkflownodes.knime.GenericNodesPlugin;
+import com.genericworkflownodes.knime.config.IPluginConfiguration;
 import com.genericworkflownodes.knime.mime.IMIMEtypeRegistry;
 import com.genericworkflownodes.knime.payload.IPayloadDirectory;
 import com.genericworkflownodes.knime.payload.OSGIBundlePayloadDirectory;
@@ -385,5 +386,12 @@ public abstract class GenericActivator extends AbstractUIPlugin {
 	 * @return Returns the {@link Class} where the binaries are located.
 	 */
 	public abstract Class<?> getBinaryLocation();
+
+	/**
+	 * Gives access to the plugin config of the derived plugin.
+	 * 
+	 * @return The plugin config.
+	 */
+	public abstract IPluginConfiguration getPluginConfiguration();
 
 }

com.genericworkflownodes.knime.base_plugin/src/com/genericworkflownodes/knime/custom/MissingBinariesDialog.java
@@ -50,6 +50,9 @@ public class MissingBinariesDialog extends Dialog {
 	 */
 	private final String m_preferencePageId;
 
+	/**
+	 * The checkbox to disable this warning.
+	 */
 	private Button btnDoNotShow;
 
 	/**
@@ -77,12 +80,14 @@ public class MissingBinariesDialog extends Dialog {
 		gl_container.marginHeight = 15;
 		container.setLayout(gl_container);
 
+		// The warning icon
 		Label lblWarning = new Label(container, SWT.NONE);
 		lblWarning.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false,
 				1, 1));
 		lblWarning.setImage(Display.getCurrent().getSystemImage(
 				SWT.ICON_WARNING));
 
+		// The textfield containing the link to the preference dialog
 		Link linkMissingBinariesPlease = new Link(container, SWT.NONE);
 		linkMissingBinariesPlease.setLayoutData(new GridData(SWT.FILL,
 				SWT.FILL, true, true, 1, 1));
@@ -102,16 +107,17 @@ public class MissingBinariesDialog extends Dialog {
 							.format("Not all nodes of the plugin %s are associated with a valid binary.",
 									m_bundleName));
 
-					// this is call is blocking
+					// Open the Pref dialog
 					prefDialog.open();
 
-					// close the window
+					// close this window
 					MissingBinariesDialog.this.close();
 				}
 			}
 		});
 		new Label(container, SWT.NONE);
 
+		// The check box for do not show again
 		btnDoNotShow = new Button(container, SWT.CHECK);
 		btnDoNotShow.setSelection(true);
 		btnDoNotShow.addSelectionListener(new SelectionAdapter() {

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/NodeGenerator.java
@@ -53,22 +53,23 @@ import com.genericworkflownodes.knime.nodegeneration.model.directories.source.Ic
 import com.genericworkflownodes.knime.nodegeneration.model.files.CTDFile;
 import com.genericworkflownodes.knime.nodegeneration.templates.BinaryResourcesTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.BuildPropertiesTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.FragmentBuildPropertiesTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.FragmentManifestMFTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.ManifestMFTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.MimeFileCellFactoryTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.NodeDialogTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.NodeFactoryTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.NodeFactoryXMLTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.NodeModelTemplate;
-import com.genericworkflownodes.knime.nodegeneration.templates.NodeViewTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.PluginActivatorTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.PluginPreferencePageTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.PluginXMLTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.ProjectTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.StartupTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.feature.FeatureBuildPropertiesTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.feature.FeatureProjectTemplate;
 import com.genericworkflownodes.knime.nodegeneration.templates.feature.FeatureXMLTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.fragment.FragmentBuildPropertiesTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.fragment.FragmentManifestMFTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.knime_node.NodeDialogTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.knime_node.NodeFactoryTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.knime_node.NodeFactoryXMLTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.knime_node.NodeModelTemplate;
+import com.genericworkflownodes.knime.nodegeneration.templates.knime_node.NodeViewTemplate;
 import com.genericworkflownodes.knime.nodegeneration.util.Utils;
 import com.genericworkflownodes.knime.nodegeneration.writer.PropertiesWriter;
 
@@ -201,6 +202,11 @@ public class NodeGenerator {
 					.write(new File(pluginBuildDir.getKnimeDirectory(),
 							"PluginActivator.java"));
 
+			// src/[PACKAGE]/knime/PluginActivator.java
+			new StartupTemplate(meta.getPackageRoot(), meta.getName())
+					.write(new File(pluginBuildDir.getKnimeDirectory(),
+							"Startup.java"));
+
 			// src/[PACKAGE]/knime/preferences/PluginPreferencePage.java
 			new PluginPreferencePageTemplate(meta.getPackageRoot())
 					.write(new File(new File(
@@ -215,6 +221,9 @@ public class NodeGenerator {
 
 			// register preference page
 			pluginXML.registerPreferencePage(meta);
+
+			// register startup
+			pluginXML.registerStartupClass(meta);
 
 			// plugin.xml
 			pluginXML.saveTo(pluginBuildDir.getPluginXml());

commit 3fb94698fe939b20e0faff7ef1ddaab72076d960

fixed small bug introduced in b85123d1520d6297e86095227614e3d876fb5cf5

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeDialogTemplate.java
@@ -11,7 +11,7 @@ public class NodeDialogTemplate extends Template {
 			throws IOException {
 		super(
 				NodeGenerator.class
-						.getResourceAsStream("templates/knime_nodes/NodeDialog.template"));
+						.getResourceAsStream("templates/knime_node/NodeDialog.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeFactoryTemplate.java
@@ -11,7 +11,7 @@ public class NodeFactoryTemplate extends Template {
 			throws IOException {
 		super(
 				NodeGenerator.class
-						.getResourceAsStream("templates/knime_nodes/NodeFactory.template"));
+						.getResourceAsStream("templates/knime_node/NodeFactory.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeFactoryXMLTemplate.java
@@ -160,7 +160,7 @@ public class NodeFactoryXMLTemplate extends Template {
 			throws IOException {
 		super(
 				NodeGenerator.class
-						.getResourceAsStream("templates/knime_nodes/NodeXMLDescriptor.template"));
+						.getResourceAsStream("templates/knime_node/NodeXMLDescriptor.template"));
 
 		replace("__ICON__", iconPath);
 		replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeModelTemplate.java
@@ -13,9 +13,8 @@ public class NodeModelTemplate extends Template {
 	public NodeModelTemplate(String packageName, String nodeName,
 			INodeConfiguration nodeConfiguration) throws IOException,
 			UnknownMimeTypeException {
-		super(
-				NodeGenerator.class
-						.getResourceAsStream("templates/knime_nodes/NodeModel.template"));
+		super(NodeGenerator.class
+				.getResourceAsStream("templates/knime_node/NodeModel.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);

com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/knime_node/NodeViewTemplate.java
@@ -10,7 +10,7 @@ public class NodeViewTemplate extends Template {
 	public NodeViewTemplate(String packageName, String nodeName)
 			throws IOException {
 		super(NodeGenerator.class
-				.getResourceAsStream("templates/knime_nodes/NodeView.template"));
+				.getResourceAsStream("templates/knime_node/NodeView.template"));
 
 		this.replace("__BASE__", packageName);
 		this.replace("__NODENAME__", nodeName);