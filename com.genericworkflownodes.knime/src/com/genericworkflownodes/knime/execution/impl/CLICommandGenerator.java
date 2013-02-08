982090d94042ce6c5be55d754eddb4397f281f03
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-02-08 18:24:54 +0100

    fixed a bug in CLICommandGenerator

--- com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/execution/impl/CLICommandGenerator.java
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/execution/impl/CLICommandGenerator.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2012, Stephan Aiche.
+ *
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.genericworkflownodes.knime.execution.impl;
 
 import java.io.File;
@@ -17,6 +35,12 @@ import com.genericworkflownodes.knime.parameter.BoolParameter;
 import com.genericworkflownodes.knime.parameter.ListParameter;
 import com.genericworkflownodes.knime.parameter.Parameter;
 
+/**
+ * Generates the command line call given a {@link INodeConfiguration} containing
+ * CLI information.
+ * 
+ * @author aiche
+ */
 public class CLICommandGenerator implements ICommandGenerator {
 
 	protected static final NodeLogger logger = NodeLogger
@@ -29,12 +53,12 @@ public class CLICommandGenerator implements ICommandGenerator {
 			IPluginConfiguration pluginConfiguration, File workingDirectory)
 			throws Exception {
 
+		// ease the passing around of variables
+		nodeConfig = nodeConfiguration;
+
 		// export the node configuration as plain text, for debugging and
 		// logging
 		exportPlainConfiguration(workingDirectory);
-
-		// ease the passing around of variables
-		nodeConfig = nodeConfiguration;
 
 		List<String> commands;
 
