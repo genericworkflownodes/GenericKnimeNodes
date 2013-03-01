c1fe031af6073f884efa9db89f7fe5baa9585ee8
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-03-01 10:05:22 +0100

    some initial code cleanup

--- com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/flow/listzip/ListZipLoopStartNodeModel.java
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/flow/listzip/ListZipLoopStartNodeModel.java
@@ -1,5 +1,6 @@
-/*
+/**
  * Copyright (c) 2011, Marc Röttig.
+ * Copyright (c) 2013, Stephan Aiche.
  *
  * This file is part of GenericKnimeNodes.
  * 
@@ -16,7 +17,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.genericworkflownodes.knime.nodes.flow.listzip;
 
 import java.io.File;
@@ -35,28 +35,53 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
 import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.port.PortType;
 import org.knime.core.node.workflow.LoopStartNodeTerminator;
 
+/**
+ * Node model for the ListZipLoopStart node.
+ * 
+ * @author aiche
+ */
 public class ListZipLoopStartNodeModel extends NodeModel implements
 		LoopStartNodeTerminator {
+
+	/**
+	 * The current iteration.
+	 */
 	private int m_iteration;
 
+	/**
+	 * The number of incoming ports that were actually connected by the user.
+	 */
+	private int m_numAssignedIncomingPorts;
+
+	/**
+	 * The actual number of iterations.
+	 */
+	private int m_rowCount = 0;
+
 	private static int NinPorts = 4;
+
+	static String CFG_RECYCLE = "recycle_1st_port";
+	static boolean DEFAULT_RECYCLE = false;
+	SettingsModelBoolean m_multiplex = new SettingsModelBoolean(CFG_RECYCLE,
+			DEFAULT_RECYCLE);
 
 	/**
 	 * Creates a new model.
 	 */
 	public ListZipLoopStartNodeModel() {
-		super(createIPOs(), createOPOs());
+		super(createIncomingPortObjects(), createOutgoingPortObjects());
 	}
 
-	public static final PortType OPTIONAL_PORT_TYPE = new PortType(
+	private static final PortType OPTIONAL_PORT_TYPE = new PortType(
 			URIPortObject.class, true);
 
-	private static PortType[] createIPOs() {
+	private static PortType[] createIncomingPortObjects() {
 		PortType[] portTypes = new PortType[NinPorts];
 		Arrays.fill(portTypes, URIPortObject.TYPE);
 		portTypes[1] = OPTIONAL_PORT_TYPE;
@@ -65,7 +90,7 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 		return portTypes;
 	}
 
-	private static PortType[] createOPOs() {
+	private static PortType[] createOutgoingPortObjects() {
 		PortType[] portTypes = new PortType[NinPorts];
 		Arrays.fill(portTypes, URIPortObject.TYPE);
 		portTypes[1] = OPTIONAL_PORT_TYPE;
@@ -91,22 +116,16 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 			specs.add(spec);
 		}
 
-		outspec = getOutSpec(specs);
-
-		return outspec;
+		return getOutputSpec(specs);
 	}
 
-	private PortObjectSpec[] outspec;
-
-	private int K;
-
-	private PortObjectSpec[] getOutSpec(List<URIPortObjectSpec> specs) {
-		K = specs.size();
+	private PortObjectSpec[] getOutputSpec(List<URIPortObjectSpec> specs) {
+		m_numAssignedIncomingPorts = specs.size();
 
 		PortObjectSpec[] ret = new PortObjectSpec[NinPorts];
 
 		for (int i = 0; i < NinPorts; i++) {
-			if (i < K) {
+			if (i < m_numAssignedIncomingPorts) {
 				ret[i] = specs.get(i);
 			} else {
 				ret[i] = null;
@@ -116,11 +135,11 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 		return ret;
 	}
 
-	private int rowCount = 0;
-
 	@Override
 	protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec)
 			throws Exception {
+
+		// check the loop conditions
 		if (m_iteration == 0) {
 			assert getLoopEndNode() == null : "1st iteration but end node set";
 		} else {
@@ -128,24 +147,25 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 		}
 
 		URIPortObject[] out = new URIPortObject[NinPorts];
-
-		rowCount = ((URIPortObject) inObjects[0]).getURIContents().size();
+		m_rowCount = ((URIPortObject) inObjects[0]).getURIContents().size();
 
 		for (int i = 0; i < NinPorts; i++) {
 			URIPortObject in = (URIPortObject) inObjects[i];
-			if (i < K) {
+			if (i < m_numAssignedIncomingPorts) {
 				URIContent uri = in.getURIContents().get(m_iteration);
-				List<URIContent> uriC = new ArrayList<URIContent>();
-				uriC.add(uri);
-				out[i] = new URIPortObject(uriC);
+				List<URIContent> uriContents = new ArrayList<URIContent>();
+				uriContents.add(uri);
+				out[i] = new URIPortObject(uriContents);
 			} else {
-				List<URIContent> uriC = new ArrayList<URIContent>();
-				out[i] = new URIPortObject(uriC);
+				out[i] = new URIPortObject(new ArrayList<URIContent>());
 			}
 		}
 
+		// TODO: check if this is necessary
 		pushFlowVariableInt("currentIteration", m_iteration);
-		pushFlowVariableInt("maxIterations", rowCount);
+		pushFlowVariableInt("maxIterations", m_rowCount);
+
+		// proceed in the number of iterations
 		m_iteration++;
 
 		return out;
@@ -157,13 +177,15 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	@Override
 	protected void reset() {
 		m_iteration = 0;
+		m_multiplex.setBooleanValue(DEFAULT_RECYCLE);
 	}
 
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean terminateLoop() {
-		boolean continueLoop = (m_iteration != rowCount);
-		return !continueLoop;
+		return m_iteration == m_rowCount;
 	}
 
 	/**
@@ -171,7 +193,7 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	 */
 	@Override
 	protected void saveSettingsTo(final NodeSettingsWO settings) {
-
+		m_multiplex.saveSettingsTo(settings);
 	}
 
 	/**
@@ -180,7 +202,7 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	@Override
 	protected void validateSettings(final NodeSettingsRO settings)
 			throws InvalidSettingsException {
-
+		m_multiplex.validateSettings(settings);
 	}
 
 	/**
@@ -189,7 +211,7 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	@Override
 	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
 			throws InvalidSettingsException {
-
+		m_multiplex.loadSettingsFrom(settings);
 	}
 
 	/**
@@ -199,7 +221,6 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	protected void loadInternals(final File nodeInternDir,
 			final ExecutionMonitor exec) throws IOException,
 			CanceledExecutionException {
-		// no internals to load
 	}
 
 	/**
@@ -209,6 +230,5 @@ public class ListZipLoopStartNodeModel extends NodeModel implements
 	protected void saveInternals(final File nodeInternDir,
 			final ExecutionMonitor exec) throws IOException,
 			CanceledExecutionException {
-		// no internals to save
 	}
 }
