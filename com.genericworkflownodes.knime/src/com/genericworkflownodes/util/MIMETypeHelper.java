4e60cd167f9eaeebbff96fa50aeafb18ae58ad7e
Stephan Aiche <stephan.aiche@fu-berlin.de>
2013-03-14 17:20:53 +0100

    added missing MIMETypeHelper

---
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/util/MIMETypeHelper.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2013, aiche.
+ *
+ * This file is part of GenericKnimeNodes.
+ * 
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.genericworkflownodes.util;
+
+import org.knime.base.filehandling.mime.MIMEMap;
+import org.knime.base.filehandling.mime.MIMETypeEntry;
+
+/**
+ * Helper class to compare and get MIMEtypes from file names.
+ * 
+ * @author aiche
+ */
+public class MIMETypeHelper {
+
+	/**
+	 * Extracts the {@link MIMETypeEntry} from the given filename.
+	 * 
+	 * @param filename
+	 *            The file for which the mime type should be extracted.
+	 * @return The MIME type of the file.
+	 */
+	public static String getMIMEtype(String filename) {
+		// check existing mimetypes
+		String type = null;
+		String foundExtension = "";
+		for (MIMETypeEntry entry : MIMEMap.getAllTypes()) {
+			for (String ext : entry.getExtensions()) {
+				if (filename.toLowerCase().endsWith(
+						"." + ext.trim().toLowerCase())
+						&& ext.length() > foundExtension.length()) {
+					type = entry.getType();
+					foundExtension = ext.trim();
+				}
+			}
+		}
+		return type;
+	}
+
+	/**
+	 * Extracts the file extension that was used to get the
+	 * {@link MIMETypeEntry} from the given filename.
+	 * 
+	 * @param filename
+	 *            The file for which the extension should be extracted.
+	 * @return The MIME type extension of the file, null if no mimetype was
+	 *         extracted.
+	 */
+	public static String getMIMEtypeExtension(String filename) {
+		// check existing mimetypes
+		String type = null;
+		String foundExtension = "";
+		for (MIMETypeEntry entry : MIMEMap.getAllTypes()) {
+			for (String ext : entry.getExtensions()) {
+				if (filename.toLowerCase().endsWith(
+						"." + ext.trim().toLowerCase())
+						&& ext.length() > foundExtension.length()) {
+					type = entry.getType();
+					foundExtension = ext.trim();
+				}
+			}
+		}
+		return (type != null ? foundExtension : null);
+	}
+}
