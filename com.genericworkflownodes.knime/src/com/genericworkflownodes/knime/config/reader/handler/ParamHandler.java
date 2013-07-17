genericworkflownodes/GenericKnimeNodes

commit bb2f4cea72e15e26036fa5da923b823a02d7a28a

GKN now supports the new CTD schema including Param 1.6.2

com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/config/reader/handler/ParamHandler.java
@@ -78,6 +78,8 @@ public class ParamHandler extends DefaultHandler {
 	private static String TYPE_FLOAT = "float";
 	private static String TYPE_DOUBLE = "double";
 	private static String TYPE_STRING = "string";
+	private static String TYPE_INPUT_FILE = "output-file";
+	private static String TYPE_OUTPUT_FILE = "input-file";
 
 	private static String ATTR_NAME = "name";
 	private static String ATTR_VALUE = "value";
@@ -86,6 +88,8 @@ public class ParamHandler extends DefaultHandler {
 	private static String ATTR_TAGS = "tags";
 	private static String ATTR_SUPPORTED_FORMATS = "supported_formats";
 	private static String ATTR_RESTRICTIONS = "restrictions";
+	private static String ATTR_ADVANCED = "advanced";
+	private static String ATTR_REQUIRED = "required";
 
 	// is contained in the schema but currently we do not handle this tag
 	@SuppressWarnings("unused")
@@ -203,7 +207,8 @@ public class ParamHandler extends DefaultHandler {
 				handleIntType(paramName, paramValue, attributes);
 			} else if (TYPE_DOUBLE.equals(type) || TYPE_FLOAT.equals(type)) {
 				handleDoubleType(paramName, paramValue, attributes);
-			} else if (TYPE_STRING.equals(type)) {
+			} else if (TYPE_STRING.equals(type) || TYPE_INPUT_FILE.equals(type)
+					|| TYPE_OUTPUT_FILE.equals(type)) {
 				handleStringType(paramName, paramValue, attributes);
 			}
 
@@ -227,7 +232,8 @@ public class ParamHandler extends DefaultHandler {
 				handleIntList(paramName, attributes);
 			} else if (TYPE_DOUBLE.equals(type) || TYPE_FLOAT.equals(type)) {
 				handleDoubleList(paramName, attributes);
-			} else if (TYPE_STRING.equals(type)) {
+			} else if (TYPE_STRING.equals(type) || TYPE_INPUT_FILE.equals(type)
+					|| TYPE_OUTPUT_FILE.equals(type)) {
 				handleStringList(paramName, attributes);
 			}
 			// initialize list for storing the list values
@@ -313,7 +319,8 @@ public class ParamHandler extends DefaultHandler {
 			((FileParameter) currentParameter).setIsOptional(p.isOptional());
 		}
 
-		if (getTags(attributes).contains(INPUTFILE_TAG)) {
+		if (attributes.getValue(ATTR_TYPE).equals(TYPE_INPUT_FILE)
+				|| getTags(attributes).contains(INPUTFILE_TAG)) {
 			inputPorts.add(p);
 		} else {
 			outputPorts.add(p);
@@ -321,8 +328,7 @@ public class ParamHandler extends DefaultHandler {
 	}
 
 	/**
-	 * Extract the list of supported FileExtensions from the given
-	 * attributes.
+	 * Extract the list of supported FileExtensions from the given attributes.
 	 * 
 	 * @param attributes
 	 *            The attributes containing the FileExtension information.
@@ -338,8 +344,7 @@ public class ParamHandler extends DefaultHandler {
 			String attrValue = attributes.getValue(ATTR_SUPPORTED_FORMATS);
 			String[] fileExtension = attrValue.split(",");
 			for (String ext : fileExtension) {
-				mimeTypes.add(ext.replaceAll("^\\s*\\*\\.", "")
-						.trim());
+				mimeTypes.add(ext.replaceAll("^\\s*\\*\\.", "").trim());
 			}
 		} else if (attributes.getValue(ATTR_RESTRICTIONS) != null
 				&& attributes.getValue(ATTR_RESTRICTIONS).length() > 0
@@ -347,8 +352,7 @@ public class ParamHandler extends DefaultHandler {
 			String attrValue = attributes.getValue(ATTR_RESTRICTIONS);
 			String[] fileExtension = attrValue.split(",");
 			for (String ext : fileExtension) {
-				mimeTypes.add(ext.replaceAll("^\\s*\\*\\.", "")
-						.trim());
+				mimeTypes.add(ext.replaceAll("^\\s*\\*\\.", "").trim());
 			}
 		}
 
@@ -426,8 +430,14 @@ public class ParamHandler extends DefaultHandler {
 
 	private boolean isPort(final Attributes attributes) {
 		Set<String> tagSet = getTags(attributes);
-		return (tagSet.contains(INPUTFILE_TAG) || tagSet
+		boolean isPort = (tagSet.contains(INPUTFILE_TAG) || tagSet
 				.contains(OUTPUTFILE_TAG));
+
+		// additionally we check if type is equal to input-file, output-file
+		isPort = attributes.getValue(ATTR_TYPE).equals(TYPE_INPUT_FILE)
+				|| attributes.getValue(ATTR_TYPE).equals(TYPE_OUTPUT_FILE);
+
+		return isPort;
 	}
 
 	private void handleIntType(final String paramName, final String paramValue,
@@ -464,12 +474,30 @@ public class ParamHandler extends DefaultHandler {
 
 	private boolean isOptional(final Attributes attributes) {
 		Set<String> tagSet = getTags(attributes);
-		return !(tagSet.contains("mandatory") || tagSet.contains("required"));
+		boolean isOptional = !(tagSet.contains("mandatory") || tagSet
+				.contains("required"));
+
+		// attribute value overrides legacy
+		if (attributes.getValue(ATTR_REQUIRED) != null) {
+			isOptional = !Boolean.parseBoolean(attributes
+					.getValue(ATTR_REQUIRED));
+		}
+
+		return isOptional;
 	}
 
 	private boolean isAdvanced(final Attributes attributes) {
+		// legacy support for advanced tag
 		Set<String> tagSet = getTags(attributes);
-		return tagSet.contains("advanced");
+		boolean isAdvanced = tagSet.contains("advanced");
+
+		// attribute value overrides legacy
+		if (attributes.getValue(ATTR_ADVANCED) != null) {
+			isAdvanced = Boolean.parseBoolean(attributes
+					.getValue(ATTR_ADVANCED));
+		}
+
+		return isAdvanced;
 	}
 
 	/**

com.genericworkflownodes.knime/test/com/genericworkflownodes/knime/config/reader/CTDConfigurationReaderTest.java
@@ -19,6 +19,7 @@
 package com.genericworkflownodes.knime.config.reader;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -47,6 +48,7 @@ public class CTDConfigurationReaderTest {
 		assertEquals("Map Alignment", config.getCategory());
 		assertNotNull(config.getParameter("FeatureLinkerUnlabeled.1.in"));
 		assertTrue(config.getParameter("FeatureLinkerUnlabeled.1.in") instanceof FileListParameter);
+		assertEquals("1.11.0", config.getVersion());
 
 		FileListParameter flp = (FileListParameter) config
 				.getParameter("FeatureLinkerUnlabeled.1.in");
@@ -60,6 +62,7 @@ public class CTDConfigurationReaderTest {
 		INodeConfiguration config = reader.read(TestDataSource.class
 				.getResourceAsStream("TMTAnalyzer.ctd"));
 		assertEquals("Quantitation", config.getCategory());
+		assertEquals("1.11.0", config.getVersion());
 		assertNotNull(config.getParameter("TMTAnalyzer.1.in"));
 		assertTrue(config.getParameter("TMTAnalyzer.1.in") instanceof FileParameter);
 
@@ -70,5 +73,15 @@ public class CTDConfigurationReaderTest {
 		assertEquals(2, slp.getValue().size());
 		assertEquals("126:liver", slp.getValue().get(0));
 		assertEquals("131:lung", slp.getValue().get(1));
+		assertTrue(config
+				.getParameter(
+						"TMTAnalyzer.1.algorithm.Quantification.isotope_correction.tmt-6plex")
+				.isAdvanced());
+		assertTrue(config
+				.getParameter(
+						"TMTAnalyzer.1.algorithm.Quantification.isotope_correction.tmt-6plex")
+				.isOptional());
+		assertFalse(config.getParameter("TMTAnalyzer.1.in").isAdvanced());
+		assertFalse(config.getParameter("TMTAnalyzer.1.in").isOptional());
 	}
 }

com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/config/reader/handler/CTDHandler.java
@@ -34,17 +34,18 @@ import com.genericworkflownodes.knime.config.NodeConfiguration;
  */
 public class CTDHandler extends DefaultHandler {
 
-	private static String TAG_NAME = "name";
-	private static String TAG_VERSION = "version";
+	private static String ATTR_NAME = "name";
+	private static String ATTR_VERSION = "version";
 	private static String TAG_DESCRIPTION = "description";
 	private static String TAG_MANUAL = "manual";
-	private static String TAG_DOCURL = "docurl";
-	private static String TAG_CATEGORY = "category";
+	private static String ATTR_DOCURL = "docurl";
+	private static String ATTR_CATEGORY = "category";
 	private static String TAG_CLI = "cli";
 	private static String TAG_RELOCATORS = "relocators";
 	private static String TAG_PARAMETERS = "PARAMETERS";
 	private static String TAG_EXECUTABLE_NAME = "executableName";
 	private static String TAG_EXECUTABLE_PATH = "executablePath";
+	private static String TAG_TOOL = "tool";
 
 	/**
 	 * The {@link INodeConfiguration} generated while parsing the CTD document.
@@ -101,32 +102,30 @@ public class CTDHandler extends DefaultHandler {
 		} else if (TAG_CLI.equals(name)) {
 			xmlReader.setContentHandler(new CLIElementHandler(xmlReader, this,
 					config));
+		} else if (TAG_TOOL.equals(name)) {
+			// root tag -> parse out the attribute values
+			config.setName(attributes.getValue(ATTR_NAME));
+			config.setVersion(attributes.getValue(ATTR_VERSION));
+			if (attributes.getValue(ATTR_DOCURL) != null) {
+				config.setDocUrl(attributes.getValue(ATTR_DOCURL));
+			}
+			if (attributes.getValue(ATTR_CATEGORY) != null) {
+				config.setCategory(attributes.getValue(ATTR_CATEGORY));
+			}
 		}
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String name)
 			throws SAXException {
-		if (TAG_NAME.equals(name)) {
-			config.setName(currentContent.toString());
-		} else if (TAG_VERSION.equals(name)) {
-			config.setVersion(currentContent.toString());
-		} else if (TAG_DESCRIPTION.equals(name)) {
+		if (TAG_DESCRIPTION.equals(name)) {
 			config.setDescription(currentContent.toString());
 		} else if (TAG_MANUAL.equals(name)) {
 			config.setManual(currentContent.toString());
-		} else if (TAG_DOCURL.equals(name)) {
-			config.setDocUrl(currentContent.toString());
-		} else if (TAG_CATEGORY.equals(name)) {
-			config.setCategory(currentContent.toString());
 		} else if (TAG_EXECUTABLE_PATH.equals(name)) {
 			config.setExecutablePath(currentContent.toString());
 		} else if (TAG_EXECUTABLE_NAME.equals(name)) {
 			config.setExecutableName(currentContent.toString());
-		} else if (TAG_RELOCATORS.equals(name)) {
-			// will not happen since we handle this element in sub handler
-		} else if (TAG_PARAMETERS.equals(name)) {
-			// will not happen since we handle this element in sub handler
 		}
 	}
 }

com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/schemas/CTD.xsd
@@ -1,194 +1,168 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Mit XMLSpy v2007 sp1 bearbeitet (http://www.altova.com) von Oliver Kohlbacher (Universität Tübingen) -->
+<!-- Draft-Version 0.3 -->
 <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-
-	<xs:include schemaLocation="Param_1_4.xsd" />
-
+	<xs:include schemaLocation="Param_1_6_2.xsd"/>
 	<xs:element name="tool">
 		<xs:complexType>
 			<xs:sequence>
-				<xs:element minOccurs="1" name="name" type="xs:string">
+				<xs:element name="description" type="xs:string" minOccurs="0">
 					<xs:annotation>
-						<xs:documentation>Name of the tool. Needs to pass following regex
-							[[A-Z]|[a-z]][[0-9]|[A-Z]|[a-z]]+
-						</xs:documentation>
+						<xs:documentation>One line description of the tool.</xs:documentation>
 					</xs:annotation>
 				</xs:element>
-				<xs:element name="executableName" type="xs:string"
-					minOccurs="0" maxOccurs="1">
+				<xs:element name="manual" type="xs:string" minOccurs="0">
 					<xs:annotation>
-						<xs:documentation>
-							Optionally a separate name for the
-							executable can
-							be specified here.
-						</xs:documentation>
+						<xs:documentation>More detailed description of the tool (about 10 lines).</xs:documentation>
 					</xs:annotation>
 				</xs:element>
-				<xs:element name="executablePath" type="xs:string"
-					minOccurs="0" maxOccurs="1">
+				<xs:element name="executableName" type="xs:string" minOccurs="0">
 					<xs:annotation>
-						<xs:documentation>Contains the path were the tool can be found
-							using either the name element or (if set) the executableName.
-						</xs:documentation>
+						<xs:documentation>Optionally a separate name for the executable can be specified here.</xs:documentation>
 					</xs:annotation>
 				</xs:element>
-				<xs:element minOccurs="1" name="version" type="xs:string">
+				<xs:element name="executablePath" type="xs:anyURI" minOccurs="0">
 					<xs:annotation>
-						<xs:documentation>
-							Version string of the tool.
-						</xs:documentation>
+						<xs:documentation>Contains the path were the tool can be found using either the name element or (if set) the executableName.</xs:documentation>
 					</xs:annotation>
 				</xs:element>
-				<xs:element minOccurs="0" name="description" type="xs:string">
+				<xs:element name="cli" type="cliType" minOccurs="0"/>
+				<xs:element name="logs" type="logCollectionType" minOccurs="0"/>
+				<xs:element name="relocators" type="relocatorCollectionType" minOccurs="0">
 					<xs:annotation>
-						<xs:documentation>
-							One line description of the tool.
-						</xs:documentation>
+						<xs:documentation>Defines rules to find the output of the tool and move it to the originally desired location.</xs:documentation>
 					</xs:annotation>
 				</xs:element>
-				<xs:element minOccurs="0" name="manual" type="xs:string">
-					<xs:annotation>
-						<xs:documentation>
-							More detailed description of the tool (about
-							10
-							lines).
-						</xs:documentation>
-					</xs:annotation>
-				</xs:element>
-				<xs:element minOccurs="0" name="docurl" type="xs:string">
-					<xs:annotation>
-						<xs:documentation>
-							URL to further documentation (WWW).
-						</xs:documentation>
-					</xs:annotation>
-				</xs:element>
-				<xs:element minOccurs="0" name="category" type="xs:string">
-					<xs:annotation>
-						<xs:documentation>
-							The category gives information on the task
-							the
-							associated tool performs. It can be used
-							to categorize different
-							tools in a GUI.
-						</xs:documentation>
-					</xs:annotation>
-				</xs:element>
-
-				<xs:element maxOccurs="1" minOccurs="0" name="cli" type="cliType">
-				</xs:element>
-
-				<xs:element name="relocators" type="relocatorCollectionType"
-					minOccurs="0" maxOccurs="1">
-					<xs:annotation>
-						<xs:documentation>
-							Defines rules to find the output of the tool and
-							move it to the
-							originally desired location.
-						</xs:documentation>
-					</xs:annotation>
-				</xs:element>
-
-				<xs:element minOccurs="1" name="PARAMETERS" type="PARAMETERSType" />
+				<xs:element name="PARAMETERS" type="PARAMETERSType"/>
 			</xs:sequence>
+			<xs:attribute name="version" type="xs:string" use="required"/>
+			<xs:attribute name="name" use="required">
+				<xs:annotation>
+					<xs:documentation>Name of the tool. Needs to pass following regex [[A-Z]|[a-z]][[0-9]|[A-Z]|[a-z]]+ i.e.\w</xs:documentation>
+				</xs:annotation>
+				<xs:simpleType>
+					<xs:restriction base="xs:string">
+						<xs:pattern value="[A-Z][A-Za-z0-9]*"/>
+					</xs:restriction>
+				</xs:simpleType>
+			</xs:attribute>
+			<xs:attribute name="docurl" type="xs:anyURI">
+				<xs:annotation>
+					<xs:documentation>URL to further documentation (WWW).</xs:documentation>
+				</xs:annotation>
+			</xs:attribute>
+			<xs:attribute name="category" type="xs:string" default="">
+				<xs:annotation>
+					<xs:documentation>The category gives information on the task the associated tool performs. It can be used to categorize different tools in a GUI.</xs:documentation>
+				</xs:annotation>
+			</xs:attribute>
 		</xs:complexType>
 	</xs:element>
 	<xs:complexType name="cliType">
 		<xs:annotation>
-			<xs:documentation>Wraps the information necessary to construct a
-				command line call for the tool.</xs:documentation>
+			<xs:documentation>Wraps the information necessary to construct a command line call for the tool.</xs:documentation>
 		</xs:annotation>
 		<xs:sequence>
-			<xs:element maxOccurs="unbounded" minOccurs="1" name="clielement"
-				type="cliElementType">
-			</xs:element>
+			<xs:element name="clielement" type="cliElementType" maxOccurs="unbounded"/>
 		</xs:sequence>
 	</xs:complexType>
 	<xs:complexType name="mappingType">
 		<xs:annotation>
-			<xs:documentation>Defines which ITEM from the parameter part is
-				associated to cliElement.</xs:documentation>
+			<xs:documentation>Defines which ITEM from the parameter part is associated to cliElement.</xs:documentation>
 		</xs:annotation>
-		<xs:attribute name="referenceName" type="xs:string">
-			<xs:annotation />
-		</xs:attribute>
+		<xs:attribute name="referenceName" type="xs:string"/>
+		<xs:attribute name="referenceID" type="xs:IDREF"/>
 	</xs:complexType>
 	<xs:complexType name="cliElementType">
 		<xs:annotation>
-			<xs:documentation>Wraps a single element of a command line call
-				(e.g., the -v flag for verbosity) and it's associated arguments. To
-				model a non-option argument just leave the optionIdentifier empty.
-			</xs:documentation>
+			<xs:documentation>Wraps a single element of a command line call (e.g., the -v flag for verbosity) and it's associated arguments. To model a non-option argument just leave the optionIdentifier empty. </xs:documentation>
 		</xs:annotation>
 		<xs:sequence>
-			<xs:element maxOccurs="unbounded" minOccurs="0" name="mapping"
-				type="mappingType">
+			<xs:element name="mapping" type="mappingType" minOccurs="0" maxOccurs="unbounded">
 				<xs:annotation>
-					<xs:documentation>The mapping defines the association between the
-						cliElement and the PARAM part.
-
-						Note: If the mapped parameter is
-						boolean, the optionIdentifier will be
-						added to the CLI only if the
-						mapped value is true.
-					</xs:documentation>
+					<xs:documentation>The mapping defines the association between the cliElement and the PARAM part. Note: If the mapped parameter is boolean, the optionIdentifier will be added to the CLI only if the mapped value is true.</xs:documentation>
 				</xs:annotation>
 			</xs:element>
 		</xs:sequence>
-		<xs:attribute name="optionIdentifier" type="xs:string"
-			use="optional">
+		<xs:attribute name="optionIdentifier" type="xs:string" use="optional">
 			<xs:annotation>
-				<xs:documentation>A string used to prefix any associated values
-					(e.g., "-v" for a boolean flag to turn on verbosity).
-				</xs:documentation>
+				<xs:documentation>A string used to prefix any associated values (e.g., "-v" for a boolean flag to turn on verbosity).</xs:documentation>
 			</xs:annotation>
 		</xs:attribute>
 		<xs:attribute name="isList" type="xs:boolean" use="optional">
 			<xs:annotation>
-				<xs:documentation>A boolean attribute indicating whether or not the
-					given cliElement can be expended to occur more then once on the
-					command line.</xs:documentation>
+				<xs:documentation>A boolean attribute indicating whether or not the given cliElement can be expended to occur more then once on the command line.</xs:documentation>
 			</xs:annotation>
 		</xs:attribute>
 		<xs:attribute name="required" type="xs:boolean" use="optional">
 			<xs:annotation>
-				<xs:documentation>Defines if the cliElement is required to build the
-					full command line, i.e., if required is set to false and the mapped
-					parameter was not set the complete element will not be used on the
-					final cli.</xs:documentation>
+				<xs:documentation>Defines if the cliElement is required to build the full command line, i.e., if required is set to false and the mapped parameter was not set the complete element will not be used on the final cli.</xs:documentation>
 			</xs:annotation>
 		</xs:attribute>
 	</xs:complexType>
-
-	<xs:complexType name="relocatorCollectionType">
+	<xs:complexType name="logCollectionType">
+		<xs:annotation>
+			<xs:documentation>Wraps the log informations from the call of the tool.</xs:documentation>
+		</xs:annotation>
 		<xs:sequence>
-			<xs:element name="relocator" type="relocatorType"></xs:element>
+			<xs:element name="log" type="logType" maxOccurs="unbounded">
+				<xs:annotation>
+					<xs:documentation>...</xs:documentation>
+				</xs:annotation>
+			</xs:element>
 		</xs:sequence>
 	</xs:complexType>
-	
-    <xs:complexType name="relocatorType">
-    	<xs:sequence>
-    	<xs:annotation>
-    	<xs:documentation>
-    	Defines a relocation rule using a pattern to find the file and the targeted output file where the found file should be moved to.
-    	</xs:documentation>
-    	</xs:annotation>
-    		<xs:element name="reference" type="xs:string" minOccurs="1" maxOccurs="1">
-    		    	<xs:annotation>
-    	<xs:documentation>
-    	The referenced parameter which should be relocated.
-    		</xs:documentation>
-    		</xs:annotation>
-    		</xs:element>
-    		<xs:element name="location" type="xs:string" minOccurs="1" maxOccurs="1">
-    		    	<xs:annotation>
-    		    		<xs:documentation>The location of the output file defined using following variables: 
-
+	<xs:complexType name="logType">
+		<xs:annotation>
+			<xs:documentation>Wraps the log informations from the call of the tool.</xs:documentation>
+		</xs:annotation>
+		<xs:sequence>
+			<xs:element name="executionWarnings" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+				<xs:annotation>
+					<xs:documentation>...</xs:documentation>
+				</xs:annotation>
+			</xs:element>
+			<xs:element name="executionErrors" type="xs:string" minOccurs="0" maxOccurs="unbounded">
+				<xs:annotation>
+					<xs:documentation>...</xs:documentation>
+				</xs:annotation>
+			</xs:element>
+			<xs:element name="executionMessage" type="xs:string" minOccurs="0" maxOccurs="unbounded"/>
+		</xs:sequence>
+		<xs:attribute name="executionTimeStart" type="xs:dateTime" use="required"/>
+		<xs:attribute name="executionTimeStop" type="xs:dateTime" use="required"/>
+		<xs:attribute name="executionStatus" type="xs:int" use="required"/>
+	</xs:complexType>
+	<xs:complexType name="logMessageType">
+		<xs:sequence>
+			<xs:element name="logMessage" type="xs:string" minOccurs="0" maxOccurs="unbounded"/>
+		</xs:sequence>
+		<xs:attribute name="NODEref" type="xs:IDREF"/>
+	</xs:complexType>
+	<xs:complexType name="relocatorCollectionType">
+		<xs:sequence>
+			<xs:element name="relocator" type="relocatorType"/>
+		</xs:sequence>
+	</xs:complexType>
+	<xs:complexType name="relocatorType">
+		<xs:sequence>
+			<xs:annotation>
+				<xs:documentation>Defines a relocation rule using a pattern to find the file and the targeted output file where the found file should be moved to.</xs:documentation>
+			</xs:annotation>
+			<xs:element name="reference" type="xs:string">
+				<xs:annotation>
+					<xs:documentation>The referenced parameter which should be relocated.</xs:documentation>
+				</xs:annotation>
+			</xs:element>
+			<xs:element name="location" type="xs:string">
+				<xs:annotation>
+					<xs:documentation>The location of the output file defined using following variables: 
 %TEMP% - the temporary directory while executing
 %PWD% - the working directory while executing 
 %BASENAME[PARAM]% - the base name of PARAM
     		</xs:documentation>
-    		    	</xs:annotation>
-
-    		</xs:element>
-    	</xs:sequence>
-    </xs:complexType>
+				</xs:annotation>
+			</xs:element>
+		</xs:sequence>
+	</xs:complexType>
 </xs:schema>

com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/schemas/Param_1_6_2.xsd
@@ -1,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified" attributeFormDefault="unqualified">
+	<xs:element name="PARAMETERS" type="PARAMETERSType">
+		<xs:annotation>
+			<xs:documentation>Main parameters node.</xs:documentation>
+		</xs:annotation>
+	</xs:element>
+	<xs:complexType name="PARAMETERSType">
+		<xs:sequence>
+			<xs:group ref="ITEMS" minOccurs="0" maxOccurs="unbounded"/>
+			<xs:element name="NODE" type="NODEType" minOccurs="0" maxOccurs="unbounded"/>
+		</xs:sequence>
+		<xs:attribute name="version" type="versionString"/>
+	</xs:complexType>
+	<xs:complexType name="AbstractITEMType" abstract="true">
+		<xs:attribute name="name" use="required">
+			<xs:simpleType>
+				<xs:restriction base="xs:string">
+					<xs:minLength value="1"/>
+				</xs:restriction>
+			</xs:simpleType>
+		</xs:attribute>
+		<xs:attribute name="type" type="PossibleITEMType" use="required">
+			<xs:annotation>
+				<xs:documentation>The type of the specified ITEM. Either input or output.</xs:documentation>
+			</xs:annotation>
+		</xs:attribute>
+		<xs:attribute name="description" type="xs:string" default=""/>
+		<xs:attribute name="tags" type="xs:string" default="false"/>
+		<xs:attribute name="restrictions" type="xs:string" default=""/>
+		<xs:attribute name="supported_formats" type="xs:string" default="false"/>
+		<xs:attribute name="required" type="xs:boolean" default="false" />
+		<xs:attribute name="advanced" type="xs:boolean" default="false" />    
+	</xs:complexType>
+	<xs:complexType name="ITEMLISTType">
+		<xs:complexContent>
+			<xs:extension base="AbstractITEMType">
+				<xs:sequence>
+					<xs:element name="LISTITEM" type="LISTITEMType" minOccurs="0" maxOccurs="unbounded"/>
+				</xs:sequence>
+			</xs:extension>
+		</xs:complexContent>
+	</xs:complexType>
+	<xs:complexType name="LISTITEMType">
+		<xs:attribute name="value" use="required"/>
+	</xs:complexType>
+	<xs:complexType name="ITEMType">
+		<xs:complexContent>
+			<xs:extension base="AbstractITEMType">
+				<xs:attribute name="value" type="xs:string" use="required"/>
+			</xs:extension>
+		</xs:complexContent>
+	</xs:complexType>
+	<xs:simpleType name="PossibleITEMType">
+		<xs:annotation>
+			<xs:documentation>Defines the possible types available in the type attribute of ITEM and ITEMLIST.</xs:documentation>
+		</xs:annotation>
+		<xs:restriction base="xs:string">
+      <xs:enumeration value="int"></xs:enumeration>
+			<xs:enumeration value="double"></xs:enumeration>
+			<xs:enumeration value="float"></xs:enumeration>      
+			<xs:enumeration value="string"></xs:enumeration>
+			<xs:enumeration value="int-pair"></xs:enumeration>
+			<xs:enumeration value="double-pair"></xs:enumeration>
+			<xs:enumeration value="output-prefix"></xs:enumeration>
+			<xs:enumeration value="input-file"></xs:enumeration>
+			<xs:enumeration value="output-file"></xs:enumeration>
+		</xs:restriction>
+	</xs:simpleType>
+	<xs:group name="ITEMS">
+		<xs:choice>
+			<xs:element name="ITEM" type="ITEMType"/>
+			<xs:element name="ITEMLIST" type="ITEMLISTType"/>
+		</xs:choice>
+	</xs:group>
+	<xs:complexType name="NODEType">
+		<xs:annotation>
+			<xs:documentation/>
+		</xs:annotation>
+		<xs:choice minOccurs="0" maxOccurs="unbounded">
+			<xs:group ref="ITEMS"/>
+			<xs:element name="NODE" type="NODEType"/>
+		</xs:choice>
+		<xs:attribute name="name" use="required">
+			<xs:simpleType>
+				<xs:restriction base="xs:string">
+					<xs:minLength value="1"/>
+				</xs:restriction>
+			</xs:simpleType>
+		</xs:attribute>
+		<xs:attribute name="description" type="xs:string" default=""/>
+	</xs:complexType>
+  <xs:simpleType name="versionString">
+    <xs:restriction base="xs:string">
+      <xs:pattern value="\d+\.\d+(\.\d+)?"></xs:pattern>
+    </xs:restriction>
+  </xs:simpleType>
+</xs:schema>

commit a6cd70e4b0e4c8fb01ce6c7ea710c23644546bf7

fixed port association bug introduced in bb2f4cea72e15e26036fa5da923b823a02d7a28a

com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/config/reader/handler/ParamHandler.java
@@ -78,8 +78,8 @@ public class ParamHandler extends DefaultHandler {
 	private static String TYPE_FLOAT = "float";
 	private static String TYPE_DOUBLE = "double";
 	private static String TYPE_STRING = "string";
-	private static String TYPE_INPUT_FILE = "output-file";
-	private static String TYPE_OUTPUT_FILE = "input-file";
+	private static String TYPE_INPUT_FILE = "input-file";
+	private static String TYPE_OUTPUT_FILE = "output-file";
 
 	private static String ATTR_NAME = "name";
 	private static String ATTR_VALUE = "value";

com.genericworkflownodes.knime/test/com/genericworkflownodes/knime/config/reader/CTDConfigurationReaderTest.java
@@ -48,6 +48,7 @@ public class CTDConfigurationReaderTest {
 		assertEquals("Map Alignment", config.getCategory());
 		assertNotNull(config.getParameter("FeatureLinkerUnlabeled.1.in"));
 		assertTrue(config.getParameter("FeatureLinkerUnlabeled.1.in") instanceof FileListParameter);
+		assertNotNull(config.getInputPortByName("FeatureLinkerUnlabeled.1.in"));
 		assertEquals("1.11.0", config.getVersion());
 
 		FileListParameter flp = (FileListParameter) config