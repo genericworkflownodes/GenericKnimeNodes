dd99b1a319c86040c228780f0657ab1ed248a3bb
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-04-25 21:05:05 +0200

    fixed formatting of p2inf file

--- com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/fragment/FragmentP2InfTemplate.java
+++ com.genericworkflownodes.knime.node_generator/src/com/genericworkflownodes/knime/nodegeneration/templates/fragment/FragmentP2InfTemplate.java
@@ -34,11 +34,9 @@ public class FragmentP2InfTemplate extends Template {
         // construct list of instructions
         StringBuilder instructions = new StringBuilder();
         for (String file : files) {
-            instructions
-                    .append(String
-                            .format("instructions.install=\\\n"
-                                    + "org.eclipse.equinox.p2.touchpoint.eclipse.chmod(targetDir:@artifact,\\\n"
-                                    + "targetFile:%s,permissions:755);\n", file));
+            instructions.append(String.format(
+                    "org.eclipse.equinox.p2.touchpoint.eclipse.chmod(targetDir:@artifact,\\\n"
+                            + "targetFile:%s,permissions:755);\n", file));
         }
 
         replace("__P2_INF_INSTRUCTIONS__", instructions.toString());
