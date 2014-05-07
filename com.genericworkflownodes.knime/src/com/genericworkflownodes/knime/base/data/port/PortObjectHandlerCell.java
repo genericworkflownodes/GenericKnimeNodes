a6aae57156536bd69fa842a4cf746d0779581074
Stephan Aiche <stephan.aiche@fu-berlin.de>
2014-05-07 14:17:03 +0200

    made PortObjectHandlerCell a proper KNIME Cell, might become handy in the future

--- com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/base/data/port/PortObjectHandlerCell.java
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/base/data/port/PortObjectHandlerCell.java
@@ -18,23 +18,67 @@
  */
 package com.genericworkflownodes.knime.base.data.port;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.knime.core.data.DataCellDataInput;
 import org.knime.core.data.DataCellDataOutput;
 import org.knime.core.data.DataCellSerializer;
 import org.knime.core.data.DataType;
+import org.knime.core.data.StringValue;
 import org.knime.core.data.filestore.FileStoreCell;
+import org.knime.core.data.uri.URIContent;
+import org.knime.core.data.uri.URIDataValue;
+import org.knime.core.node.NodeLogger;
 
 /**
  * 
  * @author aiche
  */
-public class PortObjectHandlerCell extends FileStoreCell {
+public class PortObjectHandlerCell extends FileStoreCell implements
+        URIDataValue, StringValue {
+
+    private static final NodeLogger LOGGER = NodeLogger
+            .getLogger(PortObjectHandlerCell.class);
+
+    private static final PortObjectHandlerCellSerializer SERIALIZER = new PortObjectHandlerCellSerializer();
+
+    /**
+     * DataCellSerializer for PortObjectHandlerCell.
+     * 
+     * @author aiche
+     */
+    private static final class PortObjectHandlerCellSerializer implements
+            DataCellSerializer<PortObjectHandlerCell> {
+        @Override
+        public void serialize(PortObjectHandlerCell cell,
+                DataCellDataOutput output) throws IOException {
+            cell.save(output);
+        }
+
+        @Override
+        public PortObjectHandlerCell deserialize(DataCellDataInput input)
+                throws IOException {
+            PortObjectHandlerCell cell = new PortObjectHandlerCell();
+            cell.load(input);
+            return cell;
+        }
+    }
+
     /**
      * 
      */
     private static final long serialVersionUID = -3128874200295481196L;
+
+    /**
+     * Relative path of the file inside the file store.
+     */
+    private String m_relativePath;
+
+    /**
+     * URIContent associated to the underlying file.
+     */
+    private URIContent m_uriContent;
 
     /**
      * The TYPE.
@@ -43,37 +87,93 @@ public class PortObjectHandlerCell extends FileStoreCell {
             .getType(PortObjectHandlerCell.class);
 
     /**
+     * C'tor to wrap an exisitng file store port object.
      * 
-     * @param fs
+     * @param portObject
      */
-    public PortObjectHandlerCell(AbstractFileStoreURIPortObject fs) {
-        super(fs.getInternalFileStore());
+    public PortObjectHandlerCell(AbstractFileStoreURIPortObject portObject) {
+        // we reference the file store of the portObject
+        super(portObject.getInternalFileStore());
+
+        if (portObject.getURIContents().size() > 1) {
+            LOGGER.warn("Incoming port object contains more then one file but. We will only be able to persist the first one.");
+        }
+
+        // get contents from portObject
+        m_relativePath = portObject.getRelativePaths().get(0);
+        m_uriContent = portObject.getURIContents().get(0);
     }
 
     /**
-     * 
+     * Framework c'tor.
      */
-    public PortObjectHandlerCell() {
+    PortObjectHandlerCell() {
+    }
+
+    @Override
+    protected void postConstruct() throws IOException {
+        // fix rel-path
+        File relocatedFile = new File(getFileStore().getFile(), m_relativePath);
+        if (!relocatedFile.exists()) {
+            throw new IOException(String.format(
+                    "Could not locate file %s in PortObjectHandlerCell.",
+                    m_relativePath));
+        }
+
+        // re-create uricontent
+        m_uriContent = new URIContent(relocatedFile.toURI(),
+                m_uriContent.getExtension());
     }
 
     /**
+     * Saves the content of the {@link PortObjectHandlerCell} to the given
+     * {@link DataCellDataOutput}.
      * 
-     * @return
+     * @param output
+     *            The output where the {@link PortObjectHandlerCell} should be
+     *            saved.
+     * @throws IOException
+     *             If writing fails.
+     */
+    public void save(final DataCellDataOutput output) throws IOException {
+        // save uri-content
+        m_uriContent.save(output);
+        // save rel-path
+        output.writeUTF(m_relativePath);
+    }
+
+    /**
+     * Loads the content of the {@link PortObjectHandlerCell} from the given
+     * {@link DataCellDataInput}.
+     * 
+     * @param input
+     *            The input from where the cell should be loaded.
+     * @throws IOException
+     *             If reading fails.
+     */
+    public void load(final DataCellDataInput input) throws IOException {
+        // load uri-content
+        m_uriContent = URIContent.load(input);
+        // load rel-path
+        m_relativePath = input.readUTF();
+    }
+
+    /**
+     * Access the matching DataCellSerializer.
+     * 
+     * @return The DataCellSerializer.
      */
     public static final DataCellSerializer<PortObjectHandlerCell> getCellSerializer() {
-        return new DataCellSerializer<PortObjectHandlerCell>() {
+        return SERIALIZER;
+    }
 
-            @Override
-            public void serialize(PortObjectHandlerCell cell,
-                    DataCellDataOutput output) throws IOException {
-                // do nothing
-            }
+    @Override
+    public String getStringValue() {
+        return m_uriContent.getURI().toString();
+    }
 
-            @Override
-            public PortObjectHandlerCell deserialize(DataCellDataInput input)
-                    throws IOException {
-                return new PortObjectHandlerCell();
-            }
-        };
+    @Override
+    public URIContent getURIContent() {
+        return m_uriContent;
     }
 }
