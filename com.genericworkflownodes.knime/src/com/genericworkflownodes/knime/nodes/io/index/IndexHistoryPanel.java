41f427b584405c566b170376a88fa01c8654d152
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-05-29 19:49:08 +0200

    remove usages of apache lang3 package

--- com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/io/index/IndexHistoryPanel.java
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/io/index/IndexHistoryPanel.java
@@ -1,52 +1,23 @@
+/**
+ * Copyright (c) by KNIME AG, Zurich, Switzerland & GKN team
+ *
+ * This file is part of GenericKnimeNodes.
+ *
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.genericworkflownodes.knime.nodes.io.index;
 
-/*
- * ------------------------------------------------------------------------
- *  Copyright by KNIME AG, Zurich, Switzerland
- *  Website: http://www.knime.com; Email: contact@knime.com
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License, Version 3, as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful, but
- *  WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, see <http://www.gnu.org/licenses>.
- *
- *  Additional permission under GNU GPL version 3 section 7:
- *
- *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
- *  Hence, KNIME and ECLIPSE are both independent programs and are not
- *  derived from each other. Should, however, the interpretation of the
- *  GNU GPL Version 3 ("License") under any applicable laws result in
- *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
- *  you the additional permission to use and propagate KNIME together with
- *  ECLIPSE with only the license terms in place for ECLIPSE applying to
- *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
- *  license terms of ECLIPSE themselves allow for the respective use and
- *  propagation of ECLIPSE together with KNIME.
- *
- *  Additional permission relating to nodes for KNIME that extend the Node
- *  Extension (and in particular that are based on subclasses of NodeModel,
- *  NodeDialog, and NodeView) and that only interoperate with KNIME through
- *  standard APIs ("Nodes"):
- *  Nodes are deemed to be separate and independent programs and to not be
- *  covered works.  Notwithstanding anything to the contrary in the
- *  License, the License does not apply to Nodes, you are not required to
- *  license Nodes under the License, and you are granted a license to
- *  prepare and propagate Nodes, in each case even if such Nodes are
- *  propagated with or for interoperation with KNIME.  The owner of a Node
- *  may freely choose the license terms applicable to such Node, including
- *  when such Node is propagated with or for interoperation with KNIME.
- * -------------------------------------------------------------------
- *
- * History
- *   Dec 17, 2005 (wiswedel): created
- */
 
 import java.awt.Color;
 import java.awt.Component;
@@ -100,8 +71,7 @@ import javax.swing.text.JTextComponent;
 
 import org.apache.commons.httpclient.URIException;
 import org.apache.commons.httpclient.util.URIUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.text.StrSubstitutor;
+import org.apache.commons.lang.text.StrSubstitutor;
 import org.knime.core.node.FlowVariableModel;
 import org.knime.core.node.FlowVariableModelButton;
 import org.knime.core.node.KNIMEConstants;
@@ -111,13 +81,15 @@ import org.knime.core.node.util.StringHistory;
 import org.knime.core.node.util.ViewUtils;
 import org.knime.core.util.FileUtil;
 
+
 /**
+ * <code>IndexHistoryPanel</code> for the "IndexLoader" Node.
  * Panel that contains an editable Combo Box showing the file to write to and a
  * button to trigger a file chooser. The elements in the combo are files that
  * have been recently used.
  *
  * @see org.knime.core.node.util.StringHistory
- * @author Bernd Wiswedel, University of Konstanz
+ * @author Bernd Wiswedel, University of Konstanz; Kerstin Neubert, FU Berlin
  */
 @SuppressWarnings("serial")
 public final class IndexHistoryPanel extends JPanel {
@@ -854,7 +826,10 @@ public final class IndexHistoryPanel extends JPanel {
      */
     public void setDialogTypeSaveWithExtension(final String forcedExtension) {
         setDialogType(JFileChooser.SAVE_DIALOG);
-        m_forcedFileExtensionOnSave = StringUtils.defaultIfBlank(forcedExtension, null);
+        m_forcedFileExtensionOnSave = null;
+        if (!forcedExtension.trim().isEmpty()) {
+            m_forcedFileExtensionOnSave = forcedExtension;
+        }
     }
 
     /**
@@ -867,36 +842,11 @@ public final class IndexHistoryPanel extends JPanel {
         m_connectTimeoutSpinner.getParent().setVisible(showTimeout);
     }
 
-    /**
-     * Return a file object for the given fileName. It makes sure that if the
-     * fileName is not absolute it will be relative to the user's home dir.
-     *
-     * @param fileOrUrl the file name to convert to a file
-     * @return a file representing fileName
-     * @deprecated use {@link FileUtil#resolveToPath(URL)} instead
-     */
-    @Deprecated
-    public static final File getFile(final String fileOrUrl) {
-        String path = fileOrUrl;
-        try {
-            URL u = new URL(fileOrUrl);
-            if ("file".equals(u.getProtocol())) {
-                path = u.getPath();
-            }
-        } catch (MalformedURLException ex) {
-        }
-
-        File f = new File(path);
-        if (!f.isAbsolute()) {
-            f = new File(new File(System.getProperty("user.home")), path);
-        }
-        return f;
-    }
 
     private void fileLocationChanged() {
         String selFile = getSelectedFileWithPropertiesReplaced();
         m_warnMsg.setText("");
-        if (StringUtils.isNotEmpty(selFile)) {
+        if (selFile != null && !selFile.isEmpty()) {
             try {
                 URL url = FileUtil.toURL(selFile);
                 m_warnMsg.checkLocation(url);
@@ -910,6 +860,8 @@ public final class IndexHistoryPanel extends JPanel {
         final ChangeEvent changeEvent = new ChangeEvent(this);
         m_changeListener.stream().forEach(c -> c.stateChanged(changeEvent));
     }
+    
+ 
 
     /**
      * {@inheritDoc}
@@ -1047,14 +999,7 @@ public final class IndexHistoryPanel extends JPanel {
             else {
                 extensions = IndexTypeHelper.getExtensionsByIndexType(index);
                 String ext = extensions[0]; 
-                //'lf.drp' : does not search for this extension, only 'drp' works --> cut extension before "."
-                int lastDot = ext.lastIndexOf('.') == -1 ? 0 : ext.lastIndexOf('.') + 1;
-               
-                // ext = ext.substring(lastDot);
-                
-                //ext = "." + ext;
                 filters.add(new IndexExtensionFilter(index + " Index", ext));
-         
             }    
 
             System.out.println("Index: " + index + ", extension: " + Arrays.toString(extensions));
