d6ff0c586bf36ab6789b754ca7c430445bf9e125
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-05-29 19:52:07 +0200

    add header

--- com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/io/index/IndexLoaderNodePlugin.java
+++ com.genericworkflownodes.knime/src/com/genericworkflownodes/knime/nodes/io/index/IndexLoaderNodePlugin.java
@@ -1,6 +1,20 @@
-/* @(#)$RCSfile$ 
- * $Revision$ $Date$ $Author$
+/**
+ * Copyright (c) by GKN team
  *
+ * This file is part of GenericKnimeNodes.
+ *
+ * GenericKnimeNodes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.genericworkflownodes.knime.nodes.io.index;
 
@@ -60,6 +74,7 @@ public class IndexLoaderNodePlugin extends Plugin {
     public static IndexLoaderNodePlugin getDefault() {
         return plugin;
     }
+    
 
 }
 
